a93590b4c845c34896858218a6276e37fcacfff4
unknown <codingnik20@gmail.com>
2026-04-13 23:59:55 +0530

    feat: configured feign-interceptor for jwt authentication.

--- src/main/java/com/User/Service/Configurations/FeignConfig.java
+++ src/main/java/com/User/Service/Configurations/FeignConfig.java
@@ -1,21 +1,19 @@
 package com.User.Service.Configurations;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.WebClient;
+
+import feign.RequestInterceptor;
 
 @Configuration
-public class LoadBalancedWebClientConfig {
+public class FeignConfig {
 
 	@Bean
-	@LoadBalanced
-	WebClient.Builder webClientBuilder() {
-		return WebClient.builder().filter((request, next) -> {
+	RequestInterceptor requestInterceptor() {
+		return requestTemplate -> {
 
 			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -23,13 +21,8 @@ public class LoadBalancedWebClientConfig {
 
 				String token = jwtAuth.getToken().getTokenValue();
 
-				ClientRequest newRequest = ClientRequest.from(request).header("Authorization", "Bearer " + token)
-						.build();
-
-				return next.exchange(newRequest);
+				requestTemplate.header("Authorization", "Bearer " + token);
 			}
-
-			return next.exchange(request);
-		});
+		};
 	}
 }
